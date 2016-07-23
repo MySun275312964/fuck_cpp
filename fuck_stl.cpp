8147a5773c56d54272a9c846a935a2226135f973
吴存勇 <cywu88@126.com>
2016-07-23 10:08:47 +0800

    修改文件名，并增加了list 和map的删除操作

--- fuck_stl.cpp
+++ fuck_stl.cpp
@@ -3,8 +3,12 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <vector>
 #include<algorithm>
+
+#include <vector>
+#include <list>
+#include <map>
+
 using namespace std;
 
 class A {
@@ -15,8 +19,9 @@ public:A(int i) {
 }
 };
 
-int main()
-{
+void testVector() {
+	cout << "testVector  begin...." << std::endl;
+
 	vector<int> intContainer;
 
 	for (int i = 0; i < 30; i++) {
@@ -25,10 +30,8 @@ int main()
 
 	for (int i = 0; i < 30; i++) {
 		intContainer.push_back(i);
-
 	}
 
-	 
 	cout << "intContainer.size()" << intContainer.size() << std::endl;
 	for (int i = 0; i < intContainer.size(); i++) {
 		cout << intContainer[i] << ":";
@@ -41,6 +44,7 @@ int main()
 	//	}
 	//}
 
+	//方法1  for each删除
 	//int* intValue = &(intContainer[25]);
 	//for (vector<int>::iterator it = intContainer.begin(); it != intContainer.end();)
 	//{
@@ -56,8 +60,8 @@ int main()
 	//	}
 	//}
 
+	//方法2  erase(remove)组合方式删除
 	//std::remove(intContainer.begin(), intContainer.end(), 25);
-
 	intContainer.erase(remove(intContainer.begin(), intContainer.end(), 25), intContainer.end());
 
 
@@ -80,17 +84,14 @@ int main()
 		objContainer.push_back(new A(i));
 	}
 
-	cout << "objContainer.size()" << objContainer.size() <<std::endl;
+	cout << "objContainer.size()" << objContainer.size() << std::endl;
 	for (int i = 0; i < objContainer.size(); i++) {
 		cout << objContainer[i]->i << ":";
 	}
-
 	cout << endl;
-
-	//fangfa 1
+ 
 	A* objValue = objContainer[25];
-
-
+	//方法1  for each删除
 	//for (vector<A*>::iterator it = objContainer.begin(); it != objContainer.end();)
 	//{
 	//	
@@ -104,7 +105,7 @@ int main()
 	//	}
 	//}
 
-	//fangfa 2
+	//方法2  erase(remove)组合方式删除
 	objContainer.erase(remove(objContainer.begin(), objContainer.end(), objValue), objContainer.end());
 
 
@@ -115,7 +116,89 @@ int main()
 		cout << objContainer[i]->i << ":";
 	}
 
+	cout << "testVector  end...." << std::endl;
+}
+
+//vector删除的时候只能使用 
+//it = intContainer.erase(it); 正缺
+//intContainer.erase(iter++); 这种方法会导致程序崩溃
+
+//list和map删除的时候既可以 
+// intContainer.erase(iter++);
+//也可以使用 
+//iter = intContainer.erase(iter);
+
+void testList() {
+	cout << "testList  begin...." << std::endl;
+
+	list<int> intList;
+
+	for (int i = 0; i < 30; i++) {
+		intList.push_back(i);
+	}
+
+	list<int>::iterator iter;
+
+	for (iter = intList.begin(); iter != intList.end(); iter++){
+		//cout << *iter << endl;
+		if (*iter == 3) {
+			//list使用iter++或者erase之后iter=返回值都是可以
+			//方法1
+			//intList.erase(iter++);
+			//方法2
+			iter = intList.erase(iter);
+		}
+	}
+	
+	for (iter = intList.begin(); iter != intList.end(); iter++){
+		cout << *iter << endl;
+	}
+ 
+	cout << "testList  end...." << std::endl;
+}
+
+void testMap() {
+ 
+	cout << "testMap  end...." << std::endl;
+	map<int, int> intMap;
+	for (int i = 0; i < 30; i++) {
+		intMap.insert(make_pair(i, i));
+	}
+
+	map<int, int>::iterator iter;
+	for (iter = intMap.begin(); iter !=  intMap.end(); iter++){
+		if (iter->second == 3){
+			//intMap.erase(iter++);  
+			iter = intMap.erase(iter);
+
+		}
+	}
+
+	cout << "after intMap.size()"<< intMap.size() << std::endl;
+	//for (int i = 0; i < intMap.size(); i++) {
+	//	cout << intMap[i] << endl;
+	//}
+
+	for (iter = intMap.begin(); iter != intMap.end(); iter++) {
+		cout << iter->first << " : " << iter->second << endl;
+	}
+
+	cout << "testMap  end...." << std::endl;
+//另外关于map删除会导致内存泄漏的问题 可以阅读下这篇博文
+//http://blog.csdn.net/wangandy7811/article/details/4418145
+
+}
+
+int main()
+{
+	//testVector();
+
+	//testList();
+	
+	testMap();
+
 	getchar();
-    return 0;
+
+	return 0;
 }
 
